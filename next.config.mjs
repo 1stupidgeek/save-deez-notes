87494ff4f7afaa90e9ed37efb0cf8131e865472b
ruriyoshinova <github@ruriyoshinova.dev>
2024-10-02 21:51:26 +0800

    🐳 chore: added discord.js to external packages (fixes compile issues)

--- next.config.mjs
+++ next.config.mjs
@@ -1,16 +1,22 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  webpack: (config, { isServer }) => {
-    if (isServer) {
-      config.externals.push({
-        'utf-8-validate': 'commonjs utf-8-validate',
-        'bufferutil': 'commonjs bufferutil',
-        'zlib-sync': 'commonjs zlib-sync',
-        'erlpack': 'commonjs erlpack',
-      });
-    }
-    return config;
-  },
+    experimental: {
+        serverComponentsExternalPackages: ['discord.js'],
+    },
+    webpack: (config, { isServer }) => {
+        if (isServer) {
+            config.externals.push(
+                {
+                    'utf-8-validate': 'commonjs utf-8-validate',
+                    'bufferutil': 'commonjs bufferutil',
+                    'zlib-sync': 'commonjs zlib-sync',
+                    'erlpack': 'commonjs erlpack',
+                },
+                'discord.js'
+            )
+        }
+        return config
+    },
 }
 
-export default nextConfig;
+export default nextConfig
