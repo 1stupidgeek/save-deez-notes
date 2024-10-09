5a90472b4f1981b16a135f4b6821c1997bc2e2ed
kanedaaaa <samflynn19121@gmail.com>
2024-10-09 04:31:01 +0400

    chore: check for missing env vars

---
+++ checkVars.mjs
@@ -0,0 +1,14 @@
+const vars = [
+    "KEY",
+    "DISCORD_TOKEN"
+];
+
+const checkVars = () => {
+    const missing = vars.filter((envVar) => !process.env[envVar]);
+
+    if (missing.length > 0) {
+        throw new Error(`Missing environment variables: ${missing.join(', ')}`);
+    }
+};
+
+export default checkVars;
