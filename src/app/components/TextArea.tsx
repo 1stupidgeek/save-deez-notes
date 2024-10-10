2019776a926466e53cff05546b1cc3efd3936b02
ruriyoshinova <github@ruriyoshinova.dev>
2024-10-10 09:17:27 +0800

    ✨ feat: title editor underline

--- src/app/components/TextArea.tsx
+++ src/app/components/TextArea.tsx
@@ -4,13 +4,7 @@ import { noteState } from "@/store/noteState";
 import { ChangeEvent, useCallback, useEffect, useRef, useState } from "react";
 import TextareaAutosize from "react-textarea-autosize";
 import { useRecoilState } from "recoil";
-import {
-  fetchMessage,
-  postText,
-  editText,
-  deleteText,
-  changeTitle,
-} from "@/utils/api";
+import { fetchMessage, postText, changeTitle } from "@/utils/api";
 import { debounce } from "lodash";
 
 interface Message {
@@ -24,6 +18,7 @@ export default function TextArea() {
   const [loading, setLoading] = useState(true);
   const [content, setContent] = useState("");
   const [title, setTitle] = useState("");
+  const [titleFocus, setTitleFocus] = useState(false);
 
   const textAreaRef = useRef<HTMLTextAreaElement>(null);
 
@@ -69,6 +64,7 @@ export default function TextArea() {
   };
 
   const handleTitleBlur = async () => {
+    setTitleFocus(false);
     if (title.trim() && title !== currentNote) {
       try {
         await changeTitle(currentNote, title);
@@ -88,17 +84,23 @@ export default function TextArea() {
   }
 
   return (
-    <div className="flex flex-col w-full bg-black m-10 my-12 pr-4">
-      <input
-        value={title}
-        onChange={handleTitleChange}
-        onBlur={handleTitleBlur}
-        className="resize-none border-none focus:outline-none bg-black text-white text-4xl pb-7"
-      />
+    <div className="flex flex-col gap-8 w-full bg-black mt-12 md:mt-0 p-4 md:p-12 mr-8 pr-4">
+      <div className="relative w-full">
+        <input
+          value={title}
+          onChange={handleTitleChange}
+          onFocus={() => setTitleFocus(true)}
+          onBlur={handleTitleBlur}
+          className="w-full border-none bg-transparent text-neutral-200 focus:text-neutral-50 focus:outline-none text-4xl"
+        />
+        <span
+          className={`absolute bottom-0 left-0 h-0.5 w-full bg-yellow-600 origin-left ${titleFocus ? "scale-x-100" : "scale-x-0"} transition-transform duration-300 rounded-md`}
+        ></span>
+      </div>
       <div className="flex flex-col w-full space-y-2">
         <TextareaAutosize
           placeholder="Type something ..."
-          className="flex-grow resize-none border-none focus:outline-none text-white bg-black text-lg h-full"
+          className="resize-none border-none focus:outline-none text-white bg-black text-lg h-full"
           onChange={handleContentChange}
           ref={textAreaRef}
           minRows={1}
